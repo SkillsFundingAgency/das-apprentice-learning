230b16ee271ba6641d13b8baafbfb450d5562b67
mariedale3 <marie.dale@education.gov.uk>
2020-03-26 10:36:53 +0000

    APPRL-26 Added game settings: save, reset and cancel (return to game).

---
+++ src/SFA.DAS.CaptureTheFlag.Web/Controllers/SettingsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DAS_Capture_The_Flag.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DAS_Capture_The_Flag.Controllers
+{
+    public class SettingsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // Get the settings
+
+            var model = new SettingsModel() 
+            { 
+                MusicVolume = 3,
+                SoundEffectsVolume = 3,
+                GraphicsDetail = Graphics.Medium
+            };
+
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public IActionResult Post(SettingsModel model)
+        {
+            // Save the settings
+            
+            return View("Index", model);
+        }
+    }
+}
---
+++ src/SFA.DAS.CaptureTheFlag.Web/Models/SettingsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAS_Capture_The_Flag.Models
+{
+    public class SettingsModel
+    {
+        public int MusicVolume { get; set; }
+        public int SoundEffectsVolume { get; set; }
+        public Graphics GraphicsDetail { get; set; }
+    }
+
+    public enum Graphics
+    {
+        Low, Medium, High
+    }
+
+}
+
